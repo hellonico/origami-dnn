a869f98a729e9afb5ab7d80bc1ca6775fc2a4ac6
Nicolas Modrzyk <hellonico@gmail.com>
2023-05-01 23:40:45 +0900

    opencv 4.7.0

--- project.clj
+++ project.clj
@@ -55,5 +55,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :repositories [["vendredi" "https://repository.hellonico.info/repository/hellonico/"]]
 
-  :dependencies [[org.clojure/clojure "1.10.0"] [origami/origami "4.7.0-0"]]
+  :dependencies [[org.clojure/clojure "1.10.0"] [origami/origami "4.7.0-1"]]
   :profiles {:uberjar {:aot :all}})
