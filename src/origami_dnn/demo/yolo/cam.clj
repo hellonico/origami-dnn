e9684d07694f38fe867ace046db5835ef6801f43
Nicolas Modrzyk <hellonico@gmail.com>
2026-02-18 14:52:44 +0100

    Fix origami-dnn demos: switch default networks to working ones (yolov3-tiny -> yolov2-tiny, yolov6n -> yolov6t)

--- src/origami_dnn/demo/yolo/cam.clj
+++ src/origami_dnn/demo/yolo/cam.clj
@@ -6,8 +6,8 @@
             [opencv4.utils :refer [simple-cam-window]]))
 
 (defn -main [& args]
-   (let [[net _ labels] (origami-dnn/read-net-from-repo "networks.yolo:yolov3-tiny:1.0.0")]
-  (simple-cam-window
-   (if (first args) (read-string (slurp (first args))) {:frame {:fps true} :video {:device  0}})
-   (fn [buffer]
-     (-> buffer (yolo/find-objects net) (d/blue-boxes! labels) )))))
+  (let [[net _ labels] (origami-dnn/read-net-from-repo "networks.yolo:yolov2-tiny:1.0.0")]
+    (simple-cam-window
+     (if (first args) (read-string (slurp (first args))) {:frame {:fps true} :video {:device  0}})
+     (fn [buffer]
+       (-> buffer (yolo/find-objects net) (d/blue-boxes! labels))))))
--- src/origami_dnn/demo/yolo/v6cam.clj
+++ src/origami_dnn/demo/yolo/v6cam.clj
@@ -6,14 +6,12 @@
             [opencv4.utils :refer [simple-cam-window]]))
 
 (defn -main [& args]
-  (let [
-        network-name (or (first args) "networks.yolo:yolov6n:1.0")
+  (let [network-name (or (first args) "networks.yolo:yolov6t:1.0")
         [net _ labels] (origami-dnn/read-net-from-repo network-name)]
     (simple-cam-window
-      (or (second args) {:frame {:fps true} :video {:device 0}})
-      (fn [buffer]
-        (-> buffer
-            (yolo/find-objects net)
-            (d/blue-boxes! labels)
-            (d/add-network-info! network-name)
-            ) ))))
+     (or (second args) {:frame {:fps true} :video {:device 0}})
+     (fn [buffer]
+       (-> buffer
+           (yolo/find-objects net)
+           (d/blue-boxes! labels)
+           (d/add-network-info! network-name))))))
